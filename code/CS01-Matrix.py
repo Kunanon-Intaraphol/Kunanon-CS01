ad770fcd14bc96e1f5c6a57731ccc2871aecb91c
Kunanon-Intaraphol <86783856+Kunanon-Intaraphol@users.noreply.github.com>
2021-10-20 16:22:00 +0700

    Update CS01-Matrix.py

--- code/CS01-Matrix.py
+++ code/CS01-Matrix.py
@@ -1,21 +1,21 @@
 import numpy as np
 Lenght = []
 Row = []
-m = int(input("Enter your Row: "))
-n = int(input("Enter your Lenght: "))
-v = m*n
+a = int(input("Enter your Row: "))
+b = int(input("Enter your Lenght: "))
+c = a*b
 print("Input Number first Array: ")
-for i in range(v):
+for i in range(c):
     Lenght+=[int(input(""))]
 NewRow = np.asarray(Row)
 print("Input Second Array: ")
-for a in range(v):
+for a in range(c):
     Row+=[int(input(""))]
 NewRow = np.asarray(Row)
 NewLenght = np.asarray(Lenght)
-NewRowNumpy = NewLenght.reshape(int(m),n)
+NewRowNumpy = NewLenght.reshape(int(a),b)
 print(NewRowNumpy)
-NewLenghtNumpy = NewLenght.reshape(int(n),m)
+NewLenghtNumpy = NewLenght.reshape(int(b),a)
 print(NewLenghtNumpy)
 z = np.add(NewRowNumpy,NewLenghtNumpy)
 print(z)
